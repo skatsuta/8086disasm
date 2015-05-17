2b7eeb881a49a45cd7dc8892880d72ad0aafb473
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-05-17 14:13:50 +0900

    disasm: add some fields to commond

--- disasm/disasm.go
+++ disasm/disasm.go
@@ -23,8 +23,13 @@ var (
 )
 
 type command struct {
-	c  byte
-	bs []byte
+	c    byte
+	bs   []byte
+	mnem Mnemonic
+	l    int
+	d    byte
+	w    byte
+	reg  byte
 }
 
 // Disasm is a disassembler.
@@ -146,7 +151,9 @@ type comProp struct {
 	reg byte
 }
 
-func (c *comProp) parseOp(bs []byte) error {
+func (c *command) parseOpcode(bs []byte) error {
+	c.init()
+
 	if len(bs) != 2 {
 		return errors.New("parseOp: the length of argument must be 2")
 	}
@@ -156,53 +163,53 @@ func (c *comProp) parseOp(bs []byte) error {
 	switch {
 	// add
 	case b>>2 == 0x0:
-		c.op = add
+		c.mnem = add
 		c.l = 2
 		c.d = getd(b)
 		c.w = getw(b)
 	case b>>1 == 0x2:
-		c.op = add
+		c.mnem = add
 		c.w = getw(b)
 		c.l = int(c.w + 1)
 	// push
 	case b&0xE7 == 0x6:
-		c.op = push
+		c.mnem = push
 		c.l = 1
 		c.reg = b >> 3 & 0x3
 	// pop
 	case b&0xE7 == 0x7:
-		c.op = pop
+		c.mnem = pop
 		c.l = 1
 		c.reg = b >> 3 & 0x3
 	// or
 	case b>>2 == 0x2:
-		c.op = or
+		c.mnem = or
 		c.l = 2
 		c.d = getd(b)
 		c.w = getw(b)
 	case b>>1 == 0x6:
-		c.op = or
+		c.mnem = or
 		c.l = int(c.w + 1)
 		c.w = getw(b)
 	// adc
 	case b>>2 == 0x4:
-		c.op = adc
+		c.mnem = adc
 		c.l = 2
 		c.d = getd(b)
 		c.w = getw(b)
 	// sbb
 	case b>>2 == 0x6:
-		c.op = sbb
+		c.mnem = sbb
 		c.l = 2
 		c.d = getd(b)
 		c.w = getd(b)
 	case b>>1 == 0x7:
-		c.op = sbb
+		c.mnem = sbb
 		c.l = int(c.w + 1)
 		c.w = getw(b)
 	// sub
 	case b>>2 == 0xA:
-		c.op = sub
+		c.mnem = sub
 		c.l = 2
 		c.d = getd(b)
 		c.w = getd(b)
@@ -217,4 +224,12 @@ func getd(b byte) byte {
 
 func getw(b byte) byte {
 	return b & 0x1
+}
+
+func (c *command) init() {
+	c.mnem = 0
+	c.l = 0
+	c.d = 0
+	c.w = 0
+	c.reg = 0
 }
