154de491a67cfdcb9e17d695533d07b6d89aac65
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-05-21 04:48:05 +0900

    log: add comments for methods

--- log/log.go
+++ log/log.go
@@ -15,14 +15,17 @@ type Logger interface {
 
 type logger struct{}
 
+// NewLogger returns a new logger.
 func NewLogger() Logger {
 	return logger{}
 }
 
+// Dbg logs a formatted message as a debug information.
 func (l logger) Dbg(format string, arg ...interface{}) {
 	_, _ = pp.Fprintf(os.Stderr, "[dbg] "+format, arg...)
 }
 
+// Err logs a formatted message as an error information.
 func (l logger) Err(format string, arg ...interface{}) {
 	_, _ = fmt.Fprintf(os.Stderr, "[err]"+format, arg...)
 }
